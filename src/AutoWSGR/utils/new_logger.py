b5a2acce66ac009c3afe2c33a8d417f8dba4232c
huan-yp <3051561876@qq.com>
2023-06-09 12:54:46 +0800

    修复了解装失败的 bug, 调整了 logger 获取方式
    version: 0.2.0.2

--- src/AutoWSGR/utils/new_logger.py
+++ src/AutoWSGR/utils/new_logger.py
@@ -64,7 +64,8 @@ class Logger:
         save_image(path=path, image=image, ignore_existed_image=ignore_existed_image, *args, **kwargs)
 
     def _get_logger(self, log_level="INFO") -> logging.Logger:
-        logger = logging.Logger("AutoWSGR")
+        logger = logging.getLogger("AutoWSGR")
+        logger.propagate = False
         logger.handlers = []
         logger.setLevel(log_level)
 
