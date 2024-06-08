e2cc39ff8697637c48b0779d51c7f96d927916c8
Your Name <you@example.com>
2024-06-08 15:13:00 +0800

    update pre-commit

--- develop_install.bat
+++ develop_install.bat
@@ -2,5 +2,6 @@ pip install --upgrade pip
 pip install poetry
 poetry config virtualenvs.create false --local
 poetry self add poetry-version-plugin
+poetry self update
 poetry install
 pre-commit install
