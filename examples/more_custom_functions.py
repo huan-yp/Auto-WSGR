45c1b6805263396c70a4772e677ced3bed5d3e67
veadex <65024689+veadex@users.noreply.github.com>
2024-07-19 23:29:23 +0800

    fix bug

--- examples/more_custom_functions.py
+++ examples/more_custom_functions.py
@@ -1,7 +1,9 @@
 import os
+import time
 
 import autowsgr.fight.normal_fight as nf
 import autowsgr.scripts.daily_api as da
+from autowsgr.game.build import BuildManager
 from autowsgr.game.game_operation import build, cook
 from autowsgr.scripts.main import start_script
 
@@ -26,14 +28,23 @@ def week(start=1, start_times=0, fleet_id=4, change=True):
 def day():
     # 完成日常建造, 开发, 做菜任务
     cook(timer, 3)
-    build(timer, "ship", [120, 30, 120, 30])
+    resources = [90, 30, 90, 30]
+    equipment_resources = [10, 90, 90, 30]
+    build_manager = BuildManager(timer)
+
+    while build_manager.has_empty_slot():
+        build_manager.build(resources=resources)
+        print(build_manager.slot_eta)
+
     for i in range(3):
-        build(timer, "equipment", [10, 90, 90, 30])
+        build_manager.build(type="equipment", resources=equipment_resources)
+        print(build_manager.slot_eta)
+        time.sleep(build_manager.get_timedelta().total_seconds())
 
 
 week()
 
-# day()
+day()  # 日常做菜
 
 operation = da.DailyOperation(f"{os.path.dirname(os.path.abspath(__file__))}/user_settings.yaml")
 operation.run()
