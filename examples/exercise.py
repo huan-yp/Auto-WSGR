636d01550933a78887f9ab0e969fcd8776a32c48
veadex <65024689+veadex@users.noreply.github.com>
2024-08-03 19:48:28 +0800

    :bug: 为default_settings.yaml增加OCR_BACKEND配置

--- examples/exercise.py
+++ examples/exercise.py
@@ -1,6 +1,8 @@
+import os
+
 import autowsgr.fight.exercise as ef
 from autowsgr.scripts.main import start_script
 
-timer = start_script(settings_path=None)
+timer = start_script(f"{os.path.dirname(os.path.abspath(__file__))}/user_settings.yaml")
 exf = ef.NormalExercisePlan(timer, "exercise/plan_1.yaml")
 exf.run()
