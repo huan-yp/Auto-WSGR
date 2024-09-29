aaed33431426f35955d5468ec96306e70b50de3a
veadex <65024689+veadex@users.noreply.github.com>
2024-09-29 18:46:00 +0800

    :bug: 获取舰船已掉落数据不能正确处理

--- autowsgr/game/get_game_info.py
+++ autowsgr/game/get_game_info.py
@@ -125,10 +125,10 @@ def get_loot_and_ship(timer: Timer):
             else:
                 # 如果ocr把"/"识别为"1",则使用下列方法
                 if key == "loot":
-                    ret[key] = result[1][0:-3]
+                    ret[key] = int(str(result[1])[:-3])
                     ret[key + "_max"] = 50
                 if key == "ship":
-                    ret[key] = result[1][0:-4]
+                    ret[key] = int(str(result[1])[:-4])
                     ret[key + "_max"] = 500
         else:
             timer.logger.warning(f"读取{key}数量失败")
