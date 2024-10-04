433f1d0e6679098e374dbf4be913cf939aab88cd
veadex <65024689+veadex@users.noreply.github.com>
2024-10-04 12:36:02 +0800

    plan和ship_name加载时将会采用覆盖式加载

--- examples/battle.py
+++ examples/battle.py
@@ -5,5 +5,5 @@ from autowsgr.scripts.main import start_script
 
 # 实现战役的出击
 timer = start_script(f"{os.path.dirname(os.path.abspath(__file__))}/user_settings.yaml")
-baf = bf.BattlePlan(timer, "battle/困难驱逐.yaml")
+baf = bf.BattlePlan(timer, "困难驱逐")
 baf.run()
